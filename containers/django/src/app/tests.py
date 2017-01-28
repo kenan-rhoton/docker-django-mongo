d9bb746b3d80abd0905c6dc4834a5b17d9560b26
Kenan Rhoton <kenan.rhoton@gmail.com>
2017-01-28 17:22:18 +0100

    Hacked NoSQL tests

--- containers/django/src/app/tests.py
+++ containers/django/src/app/tests.py
@@ -1,14 +1,30 @@
-from django.test.simple import DjangoTestSuiteRunner
+from django.test.simple import DiscoverRunner
 from django.test import TestCase
 
-class NoSQLTestRunner(DjangoTestSuiteRunner):
-    def setup_databases(self):
-        pass
-    def teardown_databases(self, *args):
-        pass
+class NoSQLTestRunner(DiscoverRunner):
+
+    mongodb_name = 'test_django'
+
+    def setup_databases(self, **kwargs):
+        from mongoengine.connection import connect, disconnect
+        disconnect()
+        connect(self.mongodb_name, host='mongodb://mongo/' + self.mongodb_name )
+        print('Creating mongo test database ' + self.mongodb_name)
+        # return super(MongoTestRunner, self).setup_databases(**kwargs)
+
+    def teardown_databases(self, old_config, **kwargs):
+        from mongoengine.connection import get_connection, disconnect
+        connection = get_connection()
+        connection.drop_database(self.mongodb_name)
+        print('Dropping mongo test database: ' + self.mongodb_name)
+        disconnect()
+        # super(MongoTestRunner, self).teardown_databases(old_config, **kwargs)
+
 
 class NoSQLTestCase(TestCase):
     def _fixture_setup(self):
         pass
     def _fixture_teardown(self):
-        pass
+        from mongoengine.connection import get_connection, disconnect
+        connection = get_connection()
+        connection.drop_database('test_django')
